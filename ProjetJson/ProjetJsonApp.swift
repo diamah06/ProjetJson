df14ff89d3319bba98123cd1880db7cc18df1e04
diamah06 <tabalmahdia@yahoo.fr>
2022-09-26 14:10:06 +0200

    Initial Commit

---
+++ ProjetJson/ProjetJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetJsonApp.swift
+//  ProjetJson
+//
+//  Created by Mahdia Amriou on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ProjetJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
